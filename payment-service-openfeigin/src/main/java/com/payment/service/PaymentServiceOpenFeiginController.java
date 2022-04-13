63947a8b680193c50e3e107d195064f49275dc39
qingtian <1272921450@qq.com>
2022-04-13 11:38:29 +0800

    提交nacos 配置文件动态读取

--- payment-service-openfeigin/src/main/java/com/payment/service/PaymentServiceOpenFeiginController.java
+++ payment-service-openfeigin/src/main/java/com/payment/service/PaymentServiceOpenFeiginController.java
@@ -2,9 +2,11 @@ package com.payment.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient   ////开启服务发现功能
 @EnableFeignClients    //openFeigin 开启    默认自带负载均衡
 public class PaymentServiceOpenFeiginController {
 
