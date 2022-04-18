0e5a47f2872609709e0f4f2ce2005562c3f5f75b
qingtian <1272921450@qq.com>
2022-04-18 14:32:32 +0800

    提交账户服务和商品服务配置文件

--- seata-account-service/src/main/java/com/seata/order/config/DataSourceProxyConfig.java
+++ seata-account-service/src/main/java/com/seata/order/config/DataSourceProxyConfig.java
@@ -5,15 +5,20 @@ import io.seata.rm.datasource.DataSourceProxy;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
 //自定义配置
 @Configuration
 public class DataSourceProxyConfig {
+
+    @Value("${mybatis.mapper-locations}")
+    private String mapperLocations;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
@@ -22,7 +27,7 @@ public class DataSourceProxyConfig {
     }
 
     @Bean
-    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
+    public DataSourceProxy dataSourceProxy(DataSource dataSource)  {
         return new DataSourceProxy(dataSource);
     }
 
@@ -30,6 +35,7 @@ public class DataSourceProxyConfig {
     public SqlSessionFactory sqlSessionFactoryBean(DataSourceProxy dataSourceProxy) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSourceProxy);
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations) );
         sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
         return sqlSessionFactoryBean.getObject();
     }
